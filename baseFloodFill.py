bfe1b81b7991b783bca3ec7d08b631416dbefb94
Eugenio <eugenio.onboard@gmail.com>
2020-08-31 22:29:16 -0300

    Base code to flood fill

---
+++ baseFloodFill.py
@@ -0,0 +1,46 @@
+# Dimentions of paint screen 
+M = 8
+N = 8
+
+def floodFillUtil(screen, x, y, prevC, newC): 
+	
+	# Casos Triviais
+	if (x < 0 or x >= M or y < 0 or
+		y >= N or screen[x][y] != prevC or
+		screen[x][y] == newC): 
+		return
+
+	# Recolorindo
+	screen[x][y] = newC 
+
+	# Colorido os Vizinhos
+	floodFillUtil(screen, x + 1, y, prevC, newC) 
+	floodFillUtil(screen, x - 1, y, prevC, newC) 
+	floodFillUtil(screen, x, y + 1, prevC, newC) 
+	floodFillUtil(screen, x, y - 1, prevC, newC) 
+
+# Função Inicial
+def floodFill(screen, x, y, newC): 
+	prevC = screen[x][y] 
+	floodFillUtil(screen, x, y, prevC, newC) 
+
+# "Main"
+screen = [[1, 1, 1, 1, 1, 1, 1, 1], 
+		[1, 1, 1, 1, 1, 1, 0, 0], 
+		[1, 0, 0, 1, 1, 0, 1, 1], 
+		[1, 2, 2, 2, 2, 0, 1, 0], 
+		[1, 1, 1, 2, 2, 0, 1, 0], 
+		[1, 1, 1, 2, 2, 2, 2, 0], 
+		[1, 1, 1, 1, 1, 2, 1, 1], 
+		[1, 1, 1, 1, 1, 2, 2, 1]] 
+
+x = 0
+y = 0
+newC = 3
+floodFill(screen, x, y, newC) 
+
+print ("Após aplicar o Balde de Tinta:") 
+for i in range(M): 
+	for j in range(N): 
+		print(screen[i][j], end = ' ') 
+	print()  
